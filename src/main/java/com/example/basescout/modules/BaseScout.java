e147d13ce7e51773d87c5891b99b1d1768204879
Pepit234 <hfhfhhdhft@gmail.com>
2026-08-17 18:27:00 -0400

    Delete src/main/java directory

--- src/main/java/com/example/basescout/modules/BaseScout.java
+++
@@ -1,9 +0,0 @@
-package com.example.basescout.modules;
-
-import meteordevelopment.meteorclient.systems.modules.Categories;
-import meteordevelopment.meteorclient.systems.modules.Category;
-
-public final class BaseScout {
-    public static final Category CATEGORY = Categories.World;
-    private BaseScout() {}
-}
