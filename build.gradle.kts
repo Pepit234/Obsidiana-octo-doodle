66fbdd4cc89f0d9e246ab81b40fc9a0b24de7ceb
Pepit234 <hfhfhhdhft@gmail.com>
2026-08-17 18:18:21 -0400

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,18 +6,12 @@ plugins {
 group = project.property("maven_group")!!
 version = project.property("version")!!
 
-repositories {
-    maven("https://maven.meteordev.org/snapshots")
-    maven("https://maven.meteordev.org/releases")
-    mavenCentral()
-}
-
 dependencies {
     minecraft("com.mojang:minecraft:1.21.8")
     mappings("net.fabricmc:yarn:1.21.8+build.1:v2")
     modImplementation("net.fabricmc:fabric-loader:0.16.14")
-    modImplementation("net.fabricmc.fabric-api:fabric-api:0.133.5+1.21.8")
-    modCompileOnly("meteordevelopment:meteor-client:1.21.8-SNAPSHOT")
+    modImplementation("net.fabricmc.fabric-api:fabric-api:0.133.4+1.21.8")
+    modCompileOnly(files("libs/meteor-client-1.21.8-69.jar"))
 }
 
 java {
