894e76a9c15432480bd65dae6c159672e05415e0
Pepit234 <hfhfhhdhft@gmail.com>
2026-08-17 18:26:15 -0400

    Add BaseScout module with category definition

---
+++ src/main/java/com/example/basescout/modules/BaseScout.java
@@ -0,0 +1,9 @@
+package com.example.basescout.modules;
+
+import meteordevelopment.meteorclient.systems.modules.Categories;
+import meteordevelopment.meteorclient.systems.modules.Category;
+
+public final class BaseScout {
+    public static final Category CATEGORY = Categories.World;
+    private BaseScout() {}
+}
